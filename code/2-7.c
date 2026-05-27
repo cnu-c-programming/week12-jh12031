b06660186be8ae64fbde47fb42e78399d37174b9
jh12031 <jh0100c@gmail.com>
2026-05-27 11:48:49 +0900

    2-7 수정

--- code/2-7.c
+++ code/2-7.c
@@ -10,6 +10,33 @@ typedef struct config {
 } Config;
 
 void config_parser(Config* config_ptr) {
+    FILE* fp = fopen("config.txt", "r");
+
+    if (fp == NULL)
+        return;
+
+    char line[256];
+    char key[64];
+    char value[64];
+
+    while(fgets(line, sizeof(line), fp) != NULL) {
+        sscanf(line, "%[^=]=%s", key ,value);
+
+        if(strcmp(key, "InputFileName") == 0){
+            strcpy(config_ptr->InputFileName, value);
+        }
+        else if (strcmp(key, "Options") == 0) {
+            config_ptr->Options = atoi(value);
+        }
+        else if(strcmp(key, "SectionName") == 0) {
+            strcpy(config_ptr->SectionName, value);
+        } 
+        else if(strcmp(key, "Address") == 0) {
+            sscanf(value, "%llx", &config_ptr->Address);
+        }
+    }
+
+    fclose(fp);
 
 }
 
@@ -17,11 +44,11 @@ int main(int argc, const char* argv[]) {
     Config config;
     config_parser(&config);
 
-    printf("config: %s %d %s %llu\n", 
-        config.InputFileName, 
+    printf("config: %s %d %s %llu\n",
+        config.InputFileName,
         config.Options,
         config.SectionName,
         config.Address);
+    
     return 0;
 }
-

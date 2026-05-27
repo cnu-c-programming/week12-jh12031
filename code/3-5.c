3a0f13d2be00f375a06e32e574dc529a970c86a3
jh12031 <jh0100c@gmail.com>
2026-05-27 14:40:41 +0900

    3-5 수정

--- code/3-5.c
+++ code/3-5.c
@@ -2,20 +2,22 @@
 
 int main(int argc, const char* argv[]) {
     const char* filename = "numbers.txt";
+
     FILE* fp = fopen(filename, "r");
     if (fp == NULL) {
-        return 0
+        return 0;
     }
 
     int sum = 0;
-    char buffer[64];
+    int num;
+    char buffer[256];
 
-    while (fscanf(fp, "%s", buffer) != EOF) {
-        int num;
+    while (fgets(buffer, sizeof(buffer), fp) != NULL) {
         if (sscanf(buffer, "%d", &num) == 1) {
             sum += num;
-        } else {
-            fprintf(stderr, "invalid input %s\n", buffer);
+        } 
+        else {
+            fprintf(stderr, "invalid input %s", buffer);
         }
     }
 

083ee1708c9faa8e076193b7732ebaf4b6fa85a6
jh12031 <jh0100c@gmail.com>
2026-05-27 11:10:35 +0900

    2-4, 2-5, 2-6 수정

--- code/2-6.c
+++ code/2-6.c
@@ -1,15 +1,21 @@
+
 #include <stdio.h>
 #include <string.h>
 
 int main(int argc, const char* argv[]) {
-    if (argc < 3)
-        return 0;
+    if (argc < 3) return 0;
 
     FILE* fp = fopen(argv[1], "r");
+
     const char* target_str = argv[2];
+    char line[1024];
 
+    while (fgets(line, sizeof(line), fp) != NULL) {
+        if (strstr(line, target_str) != NULL) {
+            printf("%s", line);
+        }
+    }
 
-    
     fclose(fp);
+    return 0;
 }
-

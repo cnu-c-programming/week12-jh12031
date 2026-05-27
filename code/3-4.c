ad3944169344c6345705950901629b0e5cbe56de
jh12031 <jh0100c@gmail.com>
2026-05-27 14:20:15 +0900

    3-4, 3-5 수정

--- code/3-4.c
+++ code/3-4.c
@@ -1,10 +1,38 @@
 #include <stdio.h>
 
 int main(int argc, const char* argv[]) {
+    if (argc < 3) return 0;
 
-    "error: cannot open\n"
-    "error: cannot read\n"
-    "error: cannot write\n"
+    FILE* fp_src = fopen(argv[1], "r");
+    if (fp_src == NULL) {
+        perror("error: cannot open");
+        return 1;
+    }
 
+    FILE* fp_dst = fopen(argv[2], "w");
+    if (fp_dst == NULL) {
+        perror("error: cannot open");
+        fclose(fp_src);
+        return 1;
+    }
+
+    int c;
+    while (1) {
+        c = fgetc(fp_src);
+        if (c == EOF) {
+            if (ferror(fp_src)) {
+                perror("error: cannot read");
+            }
+            break;
+        }
+
+        if (fputc(c, fp_dst) == EOF) {
+            perror("error: cannot write");
+            break;
+        }
+    }
+
+    fclose(fp_src);
+    fclose(fp_dst);
+    return 0;
 }
-
--- code/3-5.c
+++ code/3-5.c
@@ -2,17 +2,23 @@
 
 int main(int argc, const char* argv[]) {
     const char* filename = "numbers.txt";
-
     FILE* fp = fopen(filename, "r");
     if (fp == NULL) {
-        return 0;
+        return 0
     }
 
-
     int sum = 0;
+    char buffer[64];
 
+    while (fscanf(fp, "%s", buffer) != EOF) {
+        int num;
+        if (sscanf(buffer, "%d", &num) == 1) {
+            sum += num;
+        }
+    }
 
     printf("sum: %d\n", sum);
+    
     fclose(fp);
     return 0;
 }
